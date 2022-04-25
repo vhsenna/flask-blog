d828012f03c7185039e11fbbc61a7db998d282b8
Victor Senna <vhsenna@gmail.com>
2022-04-24 22:46:28 -0300

    Create a form class

--- app/forms.py
+++ app/forms.py
@@ -2,3 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from app import app
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
