c3228cd6e53ae3e9ebcc7b45e5d6d7631dc7553a
Victor Senna <vhsenna@gmail.com>
2022-04-24 22:09:50 -0300

    Reorganize files and folders

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
