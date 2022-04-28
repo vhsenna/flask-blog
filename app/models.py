2d622c81d01439bbd6a10cad1d05edb0d910541b
Victor Senna <vhsenna@gmail.com>
2022-04-28 12:36:42 -0300

    Fix typo

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from unicodedata import name
 from app import db
 from datetime import datetime
 
-class Users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
