c6b2c30f6810a5b061b893ae761d5b9c680a01a0
Victor Senna <vhsenna@gmail.com>
2022-04-28 17:24:09 -0300

    Fix  create database

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
--- flasker.py
+++ flasker.py
@@ -1,1 +1,5 @@
-from app import app
+from app import app, db
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
