29335125e43903b4eff911d74639d12ffe09cdb8
Victor Senna <vhsenna@gmail.com>
2022-04-29 02:43:52 -0300

    Import and create password hash model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import email
 from unicodedata import name
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +10,7 @@ class User(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     username = db.Column(db.String(100), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.now)
+    password_hash = db.Column(db.String(128))
 
     def __repr__(self):
         return f'<User {self.name}>'
