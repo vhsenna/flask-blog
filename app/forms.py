212216edda991af4819218d4e4445faf0fe7ab52
Victor Senna <vhsenna@gmail.com>
2022-05-02 14:01:27 -0300

    Add password to forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from app import app
 
 class NameForm(FlaskForm):
@@ -10,5 +10,7 @@ class NameForm(FlaskForm):
 class UserForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Password must match')])
+    password_hash2 = PasswordField('Confirm password', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     submit = SubmitField('Submit')
