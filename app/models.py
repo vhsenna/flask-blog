c28d70460313d3a3ef22e18f074c7843498961fe
Victor Senna <vhsenna@gmail.com>
2022-05-02 12:19:19 -0300

    Check password and hash

--- app/models.py
+++ app/models.py
@@ -18,7 +18,10 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
-        self.password_hash = generate_password_hash(password) 
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.name}>'
