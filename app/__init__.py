ba09091a40f01899597720e9b72237fc54e189f9
Victor Senna <vhsenna@gmail.com>
2022-05-04 17:55:03 -0300

    Add ckeditor to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask
+from flask_ckeditor import CKEditor
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,4 +15,6 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from app import errors, forms, routes, models
+ckeditor = CKEditor(app)
+
+from app import errors, forms, models, routes
