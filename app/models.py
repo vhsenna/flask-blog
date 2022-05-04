d53520c4960804d623bea04fe6458b0b935fd4c0
Victor Senna <vhsenna@gmail.com>
2022-05-03 23:14:22 -0300

    login_manager.user_loader

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -24,6 +24,10 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'<User {self.name}>'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
