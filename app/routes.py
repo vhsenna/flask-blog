a0377bb6b3d6ccd4591d615e73335c751393f6e3
Victor Senna <vhsenna@gmail.com>
2022-05-04 15:02:45 -0300

    Check for correct user in edit view

--- app/routes.py
+++ app/routes.py
@@ -193,12 +193,13 @@ def edit_post(id):
         db.session.commit()
         flash('Post has been updated!')
         return redirect(url_for('post', id=post.id))
-    form.title.data = post.title
-    form.slug.data = post.slug
-    # form.author.data = post.author
-    form.content.data = post.content
-    return render_template('edit_post.html',
-        form=form)
+    if current_user.id == post.poster_id:
+        form.title.data = post.title
+        form.slug.data = post.slug
+        # form.author.data = post.author
+        form.content.data = post.content
+        return render_template('edit_post.html',
+            form=form)
 
 @app.route('/post/<int:id>')
 def post(id):
